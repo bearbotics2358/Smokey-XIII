0c376343d34f187bd78aa98c25041e6a55f499e3
bearbotics2358 <bearbotics2358@gmail.com>
2020-01-07 16:25:43 -0600

    Initial Commit - Lazz (not jason)

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,15 @@
+
+#include "Robot.h" 
+
+void Robot::RobotInit() {}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+int main() { return frc::StartRobot<Robot>(); }
---
+++ src/main/include/Robot.h
@@ -0,0 +1,18 @@
+
+#pragma once
+ 
+#include <frc/TimedRobot.h>
+
+class Robot : public frc::TimedRobot {
+ public:
+  void RobotInit() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+};
