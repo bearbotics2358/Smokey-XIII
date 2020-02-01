3aac8d189ddffb9432f866cb4bb735b80133c728
Caroline <marsrovr27@gmail.com>
2020-01-31 18:20:02 -0600

    Changed stuff aGaIn - CAMM, Marsrovr

--- src/main/cpp/SwerveDrive.cpp
+++ src/main/cpp/SwerveDrive.cpp
@@ -102,24 +102,28 @@ void SwerveDrive::swerveUpdate(float xIn, float yIn, float zIn, float gyroIn, bo
 	// update speeds and angles 
 
 	if(FL_Module->adjustAngle(FL_Angle)){
-		FL_Module->adjustAngle(FL_Angle);
 		FL_Module->setDriveSpeed(-0.35 * FL_Speed);
-	} 
+	} else {
+		FL_Module->setDriveSpeed(0.35 * FL_Speed);
+	}
 
 	if(FR_Module->adjustAngle(FR_Angle)){
-		FR_Module->adjustAngle(FR_Angle);
 		FR_Module->setDriveSpeed(-0.35 * FR_Speed);
-	} 
+	} else {
+		FR_Module->setDriveSpeed(0.35 * FR_Speed);
+	}
 
  	if(BL_Module->adjustAngle(BL_Angle)){
-		BL_Module->adjustAngle(BL_Angle);
 		BL_Module->setDriveSpeed(-0.35 * BL_Speed);
-	} 
+	} else {
+		BL_Module->setDriveSpeed(0.35 * BL_Speed);
+	}
 
 	if(BR_Module->adjustAngle(BR_Angle)){
-		BR_Module->adjustAngle(BR_Angle);
 		BR_Module->setDriveSpeed(-0.35 * BR_Speed);
-	} 
+	} else {
+		BR_Module->setDriveSpeed(0.35 * BR_Speed);
+	}
 }
 
 void SwerveDrive::driveDistance(float dist, float direction)
