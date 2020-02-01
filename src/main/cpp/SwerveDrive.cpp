54434e232206a2bbfed4aa056384fb5d04d442ad
Lazzulie <>
2020-02-01 15:21:56 -0600

    Angle lock functionality save me oh go- -Lazz (help we just want the robot for 20 seconds)

--- src/main/cpp/SwerveDrive.cpp
+++ src/main/cpp/SwerveDrive.cpp
@@ -6,16 +6,147 @@ FL_Module(FL_Ptr),
 FR_Module(FR_Ptr),
 BL_Module(BL_Ptr),
 BR_Module(BR_Ptr),
-anglePID(0.02, 0, 0)
+anglePID(0.2, 0, 0),
+jenkinsTheCrabPID(0.2, 0, 0)
 {
 
 }
 
 void SwerveDrive::swerveUpdate(float xIn, float yIn, float zIn, float gyroIn, bool fieldOriented) // Swerve Kinematics - Manages each module
 {
+	crabs = false;
+	
     float xInput = xIn; // Temp variables to avoid corrupting the data
     float yInput = yIn;
     float zInput = zIn;
+
+	if(fieldOriented) // Makes joystick inputs field oriented
+	{
+		float gyroRadians = gyroIn * PI / 180; 
+		float temp = yInput * cos(gyroRadians) + xInput * sin(gyroRadians);
+		xInput = -yInput * sin(gyroRadians) + xInput * cos(gyroRadians);
+		yInput = temp;
+	}
+	
+	
+
+	float r =  sqrt((DRIVE_LENGTH * DRIVE_LENGTH) + (DRIVE_WIDTH * DRIVE_WIDTH)); // radius of the drive base
+
+	float a = xInput - zInput * (DRIVE_LENGTH / r); // temp variables to simplify math
+	float b = xInput + zInput * (DRIVE_LENGTH / r);
+	float c = yInput - zInput * (DRIVE_WIDTH / r);
+	float d = yInput + zInput * (DRIVE_WIDTH / r);
+
+	float FL_Speed = sqrt(b*b + c*c);
+    float FR_Speed = sqrt(b*b + d*d);
+    float BL_Speed = sqrt(a*a + d*d);
+    float BR_Speed = sqrt(a*a + c*c);
+
+	float FL_Angle = atan2(b,c) * 180/PI; // calculates wheel angles and converts to radians
+    float FR_Angle = atan2(b,d) * 180/PI;
+    float BL_Angle = atan2(a,c) * 180/PI;
+	float BR_Angle = atan2(a,d) * 180/PI;
+
+	if(FL_Angle < 0) 
+	{
+		FL_Angle = FL_Angle + 360;	
+	}
+
+	if(FR_Angle < 0) 
+	{
+		FR_Angle = FR_Angle + 360;	
+	}
+
+	if(BL_Angle < 0) 
+	{
+		BL_Angle = BL_Angle + 360;	
+	}
+
+	if(BR_Angle < 0) 
+	{
+		BR_Angle = BR_Angle + 360;	
+	}
+
+	float max = std::max(std::max(FR_Speed, FL_Speed), std::max(BR_Speed, BL_Speed)); // find max speed value
+
+    if(max > 1) // scale inputs respectively so no speed is greater than 1
+	{
+    	FL_Speed /= max;
+    	FR_Speed /= max;
+    	BL_Speed /= max;
+    	BR_Speed /= max;
+    }
+
+	float scalar = 1; // scalar to adjust if speed is too high
+	FL_Speed *= scalar;
+    FR_Speed *= scalar;
+    BL_Speed *= scalar;
+    BR_Speed *= scalar;
+
+	float currentFL = FL_Module->getAngle(); 
+	float currentFR = FR_Module->getAngle();
+	float currentBR = BR_Module->getAngle(); 
+	float currentBL = BL_Module->getAngle();
+	
+	float deadzoneCheck = sqrt(xIn * xIn + yIn * yIn);
+
+	if(deadzoneCheck < 0.15 && fabs(zIn) < 0.01)
+	{
+		FL_Speed = 0;
+		FR_Speed = 0;
+		BL_Speed = 0;
+		BR_Speed = 0;
+
+		FL_Angle = currentFL;
+		FR_Angle = currentFR;
+		BL_Angle = currentBL;
+		BR_Angle = currentBR;
+	}
+
+	// update speeds and angles 
+
+	if(FL_Module->adjustAngle(FL_Angle)){
+		FL_Module->setDriveSpeed(-0.35 * FL_Speed);
+	} else {
+		FL_Module->setDriveSpeed(0.35 * FL_Speed);
+	}
+
+	if(FR_Module->adjustAngle(FR_Angle)){
+		FR_Module->setDriveSpeed(-0.35 * FR_Speed);
+	} else {
+		FR_Module->setDriveSpeed(0.35 * FR_Speed);
+	}
+
+ 	if(BL_Module->adjustAngle(BL_Angle)){
+		BL_Module->setDriveSpeed(-0.35 * BL_Speed);
+	} else {
+		BL_Module->setDriveSpeed(0.35 * BL_Speed);
+	}
+
+	if(BR_Module->adjustAngle(BR_Angle)){
+		BR_Module->setDriveSpeed(-0.35 * BR_Speed);
+	} else {
+		BR_Module->setDriveSpeed(0.35 * BR_Speed);
+	}
+}
+
+float SwerveDrive::lockZ(float gyro)
+{
+	float ret = jenkinsTheCrabPID.Calculate(gyro, holdAngle);
+	return ret;
+}
+
+void SwerveDrive::crabDriveUpdate(float xIn, float yIn, float gyroIn) // 
+{
+	if(!crabs)
+	{
+		holdAngle = gyroIn;
+		crabs = true;
+	}
+
+	float xInput = xIn; // Temp variables to avoid corrupting the data
+    float yInput = yIn;
+    float zInput = lockZ(gyroIn);
 
 	if(fieldOriented) // Makes joystick inputs field oriented
 	{
